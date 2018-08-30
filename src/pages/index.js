94836098c1dca88e7bc6fe6703a57eb9dde10671
nyxmnemosyne <42790949+nyxmnemosyne@users.noreply.github.com>
2018-08-30 15:18:48 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,10 @@ import Link from 'gatsby-link'
 const IndexPage = () => (
   <div>
     <h1>Hello World!</h1>
-    <h3>I'm Rein! I'm a newbie here so.. this is quite new to me.</h3>
+    <h3>Welcome to my new Gatsby site.</h3>
+    <p>I'm Rein! I'm sort of a newbie here, I'm a graphic and web designer by profession but I do some 
+    programming at times too on my personal time. I've always wanted to join a hackathon but never really had the chance to,
+    but then here comes JAMStack, I'm looking forward to this event, hoping to learn more and broaden my horizons in programming. :)</p>
   </div>
 )
 
